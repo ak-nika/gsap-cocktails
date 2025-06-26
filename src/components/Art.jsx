404e1b0b612db3f7549f2897ac1a85bc4f8fdeeb
ak-nika <akingbayiojo@gmail.com>
2025-06-26 23:14:37 +0100

    still rubbish

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -46,7 +46,7 @@ const Art = () => {
             ))}
           </ul>
 
-          <div className="cocktail-img">
+          <div className="md:w-[60vw] w-full h-[50vh] md:h-[70vh] rounded-4xl overflow-hidden">
             <img
               src="/images/under-img.jpg"
               alt="cocktail"
