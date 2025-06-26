9f86cb729817c5f1b0bbe28d59f245636badeb3f
ak-nika <akingbayiojo@gmail.com>
2025-06-26 23:06:15 +0100

    fixed art section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { featureLists, goodLists } from "../constants";
+import gsap from "gsap";
 import { useMediaQuery } from "react-responsive";
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import { featureLists, goodLists } from "../constants/";
 
 const Art = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -21,27 +20,19 @@ const Art = () => {
     });
 
     maskTimeline
-      .to(".will-fade", {
-        opacity: 0,
-        stagger: 0.2,
-        ease: "power1.inOut",
-      })
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
       .to(".masked-img", {
         scale: 1.3,
         maskPosition: "center",
         maskSize: "400%",
         duration: 1,
-        ease: "power1.inOut",
+        ease: "power1.inOut ",
       })
-      .to("#masked-content", {
-        opacity: 1,
-        duration: 1,
-        ease: "power1.inOut",
-      });
+      .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
   });
 
   return (
-    <div id="art">
+    <div id="art" className="relative">
       <div className="container mx-auto h-full pt-20">
         <h2 className="will-fade">The ART</h2>
 
@@ -55,11 +46,11 @@ const Art = () => {
             ))}
           </ul>
 
-          <div className="cocktail-img">
+          <div className="md:w-[60vw] w-full h-[50vh] md:h-[70vh] rounded-4xl overflow-hidden">
             <img
               src="/images/under-img.jpg"
-              alt="cocktails"
-              className="abs-centers masked-img size-full object-contain"
+              alt="cocktail"
+              className="abs-center masked-img size-full object-contain"
             />
           </div>
 
@@ -79,7 +70,7 @@ const Art = () => {
           <div id="masked-content">
             <h3>Made with Craft, Poured with Passion</h3>
             <p>
-              This isn't just a drink. It's a carefully crafted moment made just
+              This isn’t just a drink. It’s a carefully crafted moment made just
               for you.
             </p>
           </div>
@@ -88,5 +79,4 @@ const Art = () => {
     </div>
   );
 };
-
 export default Art;
